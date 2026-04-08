48646912b3c26f040c08c9677dc648f24b9c89a8
Wendel-E <comerbatata32@gmail.com>
2026-04-08 19:19:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Flex menu/sidebar function
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
